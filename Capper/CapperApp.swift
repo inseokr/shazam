6a155ef3d07939a82591638a5a03f54bc89a5cd3
Justin Seo <carrier_mania@yahoo.com>
2026-02-07 15:03:43 -0800

    Initial Commit

---
+++ Capper/CapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapperApp.swift
+//  Capper
+//
+//  Created by Justin Seo on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capper
+//
+//  Created by Justin Seo on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
